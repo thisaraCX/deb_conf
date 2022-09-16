bdfe2015c7f6bcb199e944696fdfd8152debf677
thisaraWSL <thisara@controlxengineering.com.au>
2022-09-16 14:30:17 +1000

    build essential and nodejs added

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,9 @@ sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release \
+    build-essential \
+    nodejs
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
