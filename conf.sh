6969b8ff0bd28e19ddde9b7a743995d3f0897986
thisaraWSL <thisara@controlxengineering.com.au>
2022-09-16 13:55:35 +1000

    updates

--- conf.sh
+++ conf.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+
+#Setting host name
 sudo hostnamectl set-hostname deb10
+
+#Setting up SSH 
 sudo apt install openssh-server &&
 sudo systemctl start ssh
 sudo systemctl enable ssh
 
-
-
-
+#Permissions enabling
 chmod +x install_docker.sh
 
-
-
+#Installing docker
 sudo ./install_docker.sh
