2a2a4dfffda66ae0e95df852f8cbbac7accbf8c0
thisaraWSL <thisara@controlxengineering.com.au>
2022-09-16 13:08:52 +1000

    ssh set up and hostname

--- conf.sh
+++ conf.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-#docker installation - https://docs.docker.com/engine/install/debian/
-echo "hello"
-sudo apt update &&
-sudo apt upgrade 
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-docker -v
+sudo hostnamectl set-hostname deb10
+sudo apt install openssh-server &&
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+
+
+
+chmod +x install_docker.sh
+
+
+
+sudo ./install_docker.sh
